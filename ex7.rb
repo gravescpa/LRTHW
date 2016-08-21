99c2bd770bc594a6e1144b682c377e166014de65
gravescpa <susan@susangravescpa.com>
2016-08-21 19:27:36 -0400

    Exercise 7

---
+++ ex7.rb
@@ -0,0 +1,57 @@
+# Exercise 7
+
+# This prints the text string
+puts "Mary had a little lamb."
+
+# This prints the text string, along with the string snow
+puts "Its fleece was white as #{'snow'}."
+
+# This prints the text string
+puts "And everywhere that Mary went."
+
+# This print the value of the string times 10.  
+puts "." * 10 # What'd that do? It prints ten periods
+
+# This defines the variable with a value of C
+end1 = "C"
+
+# This defines the variable with a value of h
+end2 = "h"
+
+# This defines the variable with a value of e
+end3 = "e"
+
+# This defines the variable with a value of e
+end4 = "e"
+
+# This defines the variable with a value of s
+end5 = "s"
+
+# This defines the variable with a value of e
+end6 = "e"
+
+# This defines the variable with a value of B
+end7 = "B"
+
+# This defines the variable with a value of u
+end8 = "u"
+
+# This defines the variable with a value of r
+end9 = "r"
+
+# This defines the variable with a value of g
+end10 = "g"
+
+# This defines the variable with a value of e
+end11 = "e"
+
+# This defines the variable with a value of r
+end12 = "r"
+
+
+# watch that print vs. puts on this line what's it do?
+# it looks like the print function does not include a return at the end
+print end1 + end2 + end3 + end4 + end5 + end6
+
+# This prints the concatenation of the variables listed
+puts end7 + end8 + end9 + end10 + end11 + end12
