744f9c3806d01258985259c5d8ca3ece2fe20da8
Susan Graves <susan@susangravescpa.com>
2016-09-16 14:36:40 -0400

    Exercise 15 with no gets.chomp

--- ex15.rb
+++ ex15.rb
@@ -12,14 +12,14 @@ puts "Here's your file #{filename}"
 #this line prints the contents of the filename
 print txt.read
 
-#this line prints text to screen
-print "Type the filename again: "
+# #this line prints text to screen
+# print "Type the filename again: "
 
-#this line prompts for input and assigns to file_again
-file_again = $stdin.gets.chomp
+# #this line prompts for input and assigns to file_again
+# file_again = $stdin.gets.chomp
 
-#this line opens the variable file_again and assigns to txt_again
-txt_again = open(file_again)
+# #this line opens the variable file_again and assigns to txt_again
+# txt_again = open(file_again)
 
-#this line prints the contents of txt_again to the screen
-print txt_again.read
+# #this line prints the contents of txt_again to the screen
+# print txt_again.read
