1ad5f1df41fd38b84288f3569f822e11b23f90fd
Susan Graves <susan@susangravescpa.com>
2016-09-16 21:39:38 -0400

    Exercise 16 with one target.write

--- ex16.rb
+++ ex16.rb
@@ -25,12 +25,19 @@ line3 = $stdin.gets.chomp
 
 puts "I'm going to write these to the file."
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write """
+#{line1}
+#{line2}
+#{line3}
+"""
+#target.write(line1, "\n", line2, "\n", line3, "\n")
+
+# target.write(line1)
+# target.write("\n")
+# target.write(line2)
+# target.write("\n")
+# target.write(line3)
+# target.write("\n")
 
 test = open(filename)
 print test.read
